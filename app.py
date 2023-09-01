4096f7ca28bb301d26dee1549ae9333574081de1
eshaque5830 <39691289+eshaque5830@users.noreply.github.com>
2023-09-01 16:37:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown('<h1 style="text-align:center;">Exploratory Data Analysis Web Applic
 
 with st.sidebar.header("Upload your dataset (.csv)"):
     uploaded_file = st.sidebar.file_uploader("upload your file", type=["csv"])
-    st.sidebar.markdown("Get a data set from a GitHub repository: [Example CSV file](https://github.com/dataprofessor/data/blob/master/iris.csv)")
+    st.sidebar.markdown("Get a data set from a GitHub repository: [Example CSV file](https://github.com/awesomedata/awesome-public-datasets)")
     
 # Add another horizontal line
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
@@ -49,9 +49,42 @@ else:
         st.write('---')
         st.header('**Profiling Report with Pandas**')
         st_profile_report(pr)
-        
-#Footer
-st.markdown('''
-  **Copyright © 2023 Ishaque Alidad. All rights reserved.**
-  **Email:** eshaquehussain@gmail.com
-  ''', unsafe_allow_html=True)
+
+  #Footer
+import streamlit as st
+
+# Your Streamlit app content
+
+# Add some space to push the footer to the bottom
+st.write("")
+st.write("")
+
+# Add your footer content with CSS
+st.markdown(
+    """
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background-color: #f5f5f5;
+        padding: 10px 0;
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Add your footer content
+st.markdown(
+    """
+    <div class="footer">
+        <p>Copyright © 2023 Ishaque Alidad. All rights reserved.</p>
+        <p>Email: eshaquehussain@gmail.com</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
